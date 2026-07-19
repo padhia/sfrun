242042e35ed46cf8b67a0adb1de975ddecdde0dd
Paresh Adhia <padhia+git@gmail.com>
2026-07-18 22:29:43 -0400

    chore: pyrefly, use Python3

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    snowflake.url = "github:padhia/snowflake/next";
-    sfconn.url = "github:padhia/sfconn/next";
-    yappt.url = "github:padhia/yappt/next";
+    snowflake.url = "github:padhia/snowflake";
+    sfconn.url = "github:padhia/sfconn";
+    yappt.url = "github:padhia/yappt";
 
     snowflake.inputs.nixpkgs.follows = "nixpkgs";
     sfconn.inputs.nixpkgs.follows = "nixpkgs";
@@ -17,64 +17,78 @@
     sfconn.inputs.snowflake.follows = "snowflake";
   };
 
-  outputs = { self, nixpkgs, flake-utils, sfconn, snowflake, yappt }:
-  let
-    inherit (nixpkgs.lib) composeManyExtensions;
-
-    overlays.default =
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      sfconn,
+      snowflake,
+      yappt,
+    }:
     let
-      pkgOverlay = final: prev: {
-        sfrun = final.python313Packages.sfrun;
+      inherit (nixpkgs.lib) composeManyExtensions;
 
-        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-          (py-final: py-prev: {
-            sfrun = py-final.callPackage ./sfrun.nix {};
-          })
+      overlays.default =
+        let
+          pkgOverlay = final: prev: {
+            sfrun = final.python3Packages.sfrun;
+
+            pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+              (py-final: py-prev: {
+                sfrun = py-final.callPackage ./sfrun.nix { };
+              })
+            ];
+          };
+        in
+        composeManyExtensions [
+          yappt.overlays.default
+          sfconn.overlays.default
+          snowflake.overlays.default
+          pkgOverlay
         ];
-      };
-    in composeManyExtensions [
-      yappt.overlays.default
-      sfconn.overlays.default
-      snowflake.overlays.default
-      pkgOverlay
-    ];
 
-    eachSystem = system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-        overlays = [ self.overlays.default ];
-      };
+      eachSystem =
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [ self.overlays.default ];
+          };
 
-      pyPkgs = pkgs.python313Packages;
+          pyPkgs = pkgs.python3Packages;
 
-      devShells.default = pkgs.mkShell {
-        name = "sfrun";
-        venvDir = "./.venv";
-        buildInputs = [
-          pkgs.ruff
-          pkgs.uv
-          pkgs.pyrefly
-          pyPkgs.python
-          pyPkgs.venvShellHook
-          pyPkgs.pytest
-          pyPkgs.yappt
-          pyPkgs.sfconn
-          pyPkgs.snowflake-snowpark-python
-        ];
-      };
+          devShells.default = pkgs.mkShell {
+            name = "sfrun";
+            venvDir = "./.venv";
+            buildInputs = [
+              pkgs.ruff
+              pkgs.uv
+              pkgs.pyrefly
+              pyPkgs.python
+              pyPkgs.venvShellHook
+              pyPkgs.pytest
+              pyPkgs.yappt
+              pyPkgs.sfconn
+              pyPkgs.snowflake-snowpark-python
+            ];
+          };
 
-      packages.default = pkgs.sfrun;
+          packages.default = pkgs.sfrun;
 
-      apps.default = {
-        type = "app";
-        program = "${packages.default}/bin/sfrun";
-      };
-    in { inherit devShells packages apps; };
+          apps.default = {
+            type = "app";
+            program = "${packages.default}/bin/sfrun";
+          };
+        in
+        {
+          inherit devShells packages apps;
+        };
 
-  in {
-    inherit overlays;
-    inherit (flake-utils.lib.eachDefaultSystem eachSystem) devShells packages apps;
-  };
+    in
+    {
+      inherit overlays;
+      inherit (flake-utils.lib.eachDefaultSystem eachSystem) devShells packages apps;
+    };
 }
